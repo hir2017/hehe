45ffb47334d2455c4daa9ff12fa37b6f40268231
liying.chen <liying.chen@asiainnovations.com>
2018-05-17 11:29:41 +0800

    JS数字格式化方式：支持向上、向下、四舍五入取N位小数

--- js/lib/util/number.js
+++ js/lib/util/number.js
@@ -104,10 +104,8 @@ const NumberUtil = {
             return 0;
         }
         let length = parseInt(num, 10);
-        
-        number = Number(number);
 
-        number = number.toFixed(length); // 保留小时后N位，四舍五入
+        number = this.asDecimal(number, length); // 保留小时后N位，四舍五入
 
         number = this.separate(number); // ,分隔符号数字
 
@@ -118,18 +116,63 @@ const NumberUtil = {
         if (number === '敬请期待') {
             return '敬请期待';
         }
-        
+
         if (!number) {
             return 0;
         }
-        let length = parseInt(num, 10);
-        
-        number = Number(number);
 
-        number = number.toFixed(length);
+        let length = parseInt(num, 10);
+
+        number = this.asDecimal(number, length);
 
         return number;
+    },
+    /**
+     * 保留小数位
+     * roundtag:舍入参数，默认 "ceil" 向上取, "floor"向下取, "round" 四舍五入
+     */
+    asDecimal(number, decimals = 2, roundtag = 'floor') {
+        number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+        roundtag = roundtag || "floor"; 
+
+        var n = !isFinite(+number) ? 0 : +number,
+            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+            sep =  ',',
+            dec = '.',
+            s = '';
+
+        var toFixedFix = function(n, prec) {
+
+            var k = Math.pow(10, prec);
+            console.log();
+
+            return '' + parseFloat(Math[roundtag](parseFloat((n * k).toFixed(prec * 2))).toFixed(prec * 2)) / k;
+        };
+
+        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+        var re = /(-?\d+)(\d{3})/;
+
+        while (re.test(s[0])) {
+            s[0] = s[0].replace(re, "$1" + sep + "$2");
+        }
+
+        if ((s[1] || '').length < prec) {
+            s[1] = s[1] || '';
+            s[1] += new Array(prec - s[1].length + 1).join('0');
+        }
+
+        return s.join(dec);
+    },
+    /**
+     * 百分数
+     */
+    asPercent(value, decimals = 0) {
+        value *= 100;
+
+        return value.toFixed(decimals) + '%'
     }
 };
+
+window.NumberUtil = NumberUtil;
 
 export default NumberUtil;
