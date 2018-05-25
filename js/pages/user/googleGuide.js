356323c083239f0171db36874a07d5c8fa461e59
xiaxiangfeng <xiangfeng_xia@suhui.com>
2018-05-25 18:12:35 +0800

    slide

--- js/pages/user/googleGuide.js
+++ js/pages/user/googleGuide.js
@@ -5,27 +5,46 @@
  */
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Carousel } from 'antd'
+import { Carousel, Icon } from 'antd'
 import google1 from '../../../images/google1.jpg'
 import google2 from '../../../images/google2.jpg'
 import google3 from '../../../images/google3.jpg'
 
 @observer
 class GoogleGuide extends Component {
+
+  constructor () {
+    super()
+    this.previous = this.previous.bind(this)
+    this.next = this.next.bind(this)
+  }
+
+  previous () {
+    this.refs.carousel.prev()
+  }
+
+  next () {
+    this.refs.carousel.next()
+  }
+  
   render() {
     return (
       <div>
         <div className="google-auth-title">
           {UPEX.lang.template('google认证指南')}
         </div>
-        <Carousel>
+        <Carousel ref="carousel">
           <div className="google-guide-img-box">
             <img src={google1} />
+            <span onClick={this.next} className="right"><Icon type="right" /></span>
           </div>
-          <div>
+          <div className="google-guide-img-box">
+            <span onClick={this.previous} className="left"><Icon type="left" /></span>
             <img src={google2} />
+            <span onClick={this.next} className="right"><Icon type="right" /></span>
           </div>
-          <div>
+          <div className="google-guide-img-box">
+            <span onClick={this.previous} className="left"><Icon type="left" /></span>
             <img src={google3} />
           </div>
         </Carousel>
