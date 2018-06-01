67e4d3b6138ecac0329e0762cf79e20187ab5baf
xiaxiangfeng <xiangfeng_xia@suhui.com>
2018-06-01 12:15:43 +0800

    修改绑定手机

--- js/mods/bindingPhone/modifyPhone.js
+++ js/mods/bindingPhone/modifyPhone.js
@@ -5,55 +5,69 @@
  */
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Button, message } from 'antd'
+import { Button, message, Select } from 'antd'
 import { Link } from 'react-router'
 import Vcodebutton from '../common/sendAuthCodeBtn'
+const Option = Select.Option;
 
-@inject('userInfoStore', 'captchaStore')
+@inject('userInfoStore', 'captchaStore', 'userStore')
 @observer
 export default class ModifyPhone extends Component {
 
-  constructor () {
+  constructor() {
     super()
+    this.onAreaCodeChange = this.onAreaCodeChange.bind(this)
     this.phoneChange = this.phoneChange.bind(this)
     this.ivCodeChange = this.ivCodeChange.bind(this)
     this.submit = this.submit.bind(this)
     this.vCodeChange = this.vCodeChange.bind(this)
+    this.captchaChange = this.captchaChange.bind(this)
   }
 
-  componentWillMount () {
+  componentWillMount() {
+    this.props.userInfoStore.getUserInfo()
     this.props.captchaStore.fetch()
+    this.setState({
+      areacode: this.props.userStore.selectedCountry.areacode
+    })
   }
 
   state = {
     phone: '',
     vCode: '',
-    ivCode: ''
+    ivCode: '',
+    areacode: ''
   }
 
-  phoneChange (e) {
+  onAreaCodeChange (val) {
+    this.setState({
+      areacode: this.props.userStore.countries[val].areacode
+    })
+  }
+
+  phoneChange(e) {
     this.setState({
       phone: e.target.value
     })
   }
 
-  vCodeChange (e) {
+  vCodeChange(e) {
     this.setState({
       vCode: e.target.value
     })
   }
 
-  ivCodeChange (e) {
+  ivCodeChange(e) {
     this.setState({
       ivCode: e.target.value
     })
   }
 
-  captchaChange () {
+  captchaChange() {
     this.props.captchaStore.fetch()
   }
 
-  submit () {
+  submit() {
     const codeid = this.props.captchaStore.codeid
     if (!this.state.phone) {
       message.error(UPEX.lang.template('新手机号不能为空'))
@@ -68,15 +82,26 @@ export default class ModifyPhone extends Component {
   }
 
   render() {
+    const userInfo = this.props.userInfoStore.userInfo || {}
     const loading = this.props.userInfoStore.submit_loading
     const codeid = this.props.captchaStore.codeid
     const captcha = this.props.captchaStore.captcha
+    const userStore = this.props.userStore
+    let options = [];
+    $.map(userStore.countries, (item, key) => {
+      options[options.length] = <Option value={key} key={key}>{UPEX.lang.template(key)}(+{item.areacode})</Option>
+    })
     return (
       <div>
         <div className="modify-password-title">
           {UPEX.lang.template('修改绑定手机')}
         </div>
         <div className="modify-password-box">
+          <div className="item-area">
+            <Select size="large" style={{ width: 360 }} onChange={this.onAreaCodeChange} defaultValue={userStore.selectedCountry.code}>
+              {options}
+            </Select>
+          </div>
           <div className="item">
             <span className="lable">{UPEX.lang.template('新手机号')}</span>
             <input onChange={this.phoneChange} className="input" />
@@ -89,13 +114,20 @@ export default class ModifyPhone extends Component {
             <img onClick={this.captchaChange} src={captcha} />
           </div>
           <div className="item v-code">
+            <span className="lable">{UPEX.lang.template('新短信确认码')}</span>
+            <input style={{width: 130}} onChange={this.vCodeChange} className="input" />
+          </div>
+          <div className="item v-code-button">
+            <Vcodebutton phone={this.state.phone} areacode={this.state.areacode} bind={true} type={2} imgCode={this.state.ivCode} codeid={codeid} />
+          </div>
+          <div className="item v-code">
             <span className="lable">{UPEX.lang.template('短信确认码')}</span>
             <input onChange={this.vCodeChange} className="input" />
           </div>
           <div className="item v-code-button">
-            <Vcodebutton bind={true} type={1} imgCode={this.state.ivCode} codeid={codeid} />
+          <Vcodebutton phone={userInfo.phone} areacode={this.state.areacode} bind={true} type={1} imgCode={this.state.ivCode} codeid={codeid} />
           </div>
-          <div className="massage" style={{display: 'none'}}>
+          <div className="massage" style={{ display: 'none' }}>
             {UPEX.lang.template('不方便接短信？可使用')}&nbsp;&nbsp;&nbsp;&nbsp;<Link>Google{UPEX.lang.template('驗證碼')}</Link>
           </div>
           <div className="submit">
