b390bbc584d4d29195c5673c184560f4938cebe6
xiaxiangfeng <xiangfeng_xia@suhui.com>
2018-06-02 20:04:55 +0800

    绑定银行UI调整

--- js/mods/bankInfo/index.js
+++ js/mods/bankInfo/index.js
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import { Select, Button, Icon, message, Upload } from 'antd';
 import BankList from './banklist.json'
+import banckCardImg from '../../../images/bank-card.jpg'
 const Option = Select.Option;
 
 const props = {
@@ -122,11 +123,16 @@ export default class BindingBank extends Component {
         </div>
         <div className="binding-bank-message">
           <ul>
-            <li>行賬戶一旦新增就不能修改，請在提交前再三確認您的銀行賬戶是否正確，如有任何問題請聯繫我們；</li>
-            <li>當設定銀行賬號之後，我們會在銀行賬號上存入一元新台幣來確定賬號是否正確，这個過程大致需要三個工作日；</li>
-            <li>請按照您開戶的信息正確填寫姓名、賬戶號碼信息，并選擇正確開戶的分行名稱。有些銀行銀行會使用分行代碼中的三碼或四碼作為賬戶的開頭，這些也是需要填寫的，請不要忽略掉；</li>
-            <li>請優先依照分行代碼(並不是分行名稱)選取銀行分行，因為銀行可能隨時更改名稱，所以分行名稱並不一定準確。如果您的分行代碼並不在我們的列表中，請與我們聯繫，切勿自行選擇其他分行。</li>
+            <li>{UPEX.lang.template('行賬戶一旦新增就不能修改，請在提交前再三確認您的銀行賬戶是否正確，如有任何問題請聯繫我們；')}</li>
+            <li>{UPEX.lang.template('當設定銀行賬號之後，我們會在銀行賬號上存入一元新台幣來確定賬號是否正確，这個過程大致需要三個工作日；')}</li>
+            <li>{UPEX.lang.template('請按照您開戶的信息正確填寫姓名、賬戶號碼信息，并選擇正確開戶的分行名稱。有些銀行銀行會使用分行代碼中的三碼或四碼作為賬戶的開頭，這些也是需要填寫的，請不要忽略掉；')}</li>
+            <li>{UPEX.lang.template('請優先依照分行代碼(並不是分行名稱)選取銀行分行，因為銀行可能隨時更改名稱，所以分行名稱並不一定準確。如果您的分行代碼並不在我們的列表中，請與我們聯繫，切勿自行選擇其他分行。')}</li>
           </ul>
+          <div>
+            <span className="banck-card-title">{UPEX.lang.template('事例图片')}</span>
+            <img src={banckCardImg} />
+            <div className="banck-card-message">{UPEX.lang.template('上傳的文件格式必須是jpg、png、jpeg 文件大小控制在10MB以内')}</div>
+          </div>
         </div>
       </div>
     )
