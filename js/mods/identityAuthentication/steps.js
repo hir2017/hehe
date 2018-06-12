5d91d83627b4592ff9875fb85ed31acdcf205a7f
liying.chen <liying.chen@asiainnovations.com>
2018-06-12 17:33:03 +0800

    ci

--- js/mods/identityAuthentication/steps.js
+++ js/mods/identityAuthentication/steps.js
@@ -23,12 +23,12 @@ export default class Steps extends Component {
   }
 
   render() {
-    const stpe = this.props.step
+    const step = this.props.step
     return (
       <div>
         <div className="one-step step">
           <span>
-            <img src={stpe === 1 ? no_oneStep : oneStep} />
+            <img src={step === 1 ? no_oneStep : oneStep} />
           </span>
           <span>{UPEX.lang.template('基本信息')}</span>
         </div>
@@ -37,7 +37,7 @@ export default class Steps extends Component {
         }
         <div className="two-step step">
           <span>
-            <img src={(stpe === 3 || stpe === 4) ? twoStep : no_twoStep} />
+            <img src={(step === 3 || step === 4) ? twoStep : no_twoStep} />
           </span>
           <span>{UPEX.lang.template('身份认证')}</span>
         </div>
@@ -46,7 +46,7 @@ export default class Steps extends Component {
         }
         <div className="three-step step">
           <span>
-            <img src={stpe === 4 ? threeStep : no_threeStep} />
+            <img src={step === 4 ? threeStep : no_threeStep} />
           </span>
           <span>{UPEX.lang.template('安成认证')}</span>
         </div>
