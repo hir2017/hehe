163760cc41523789bfae99fe7661d611c1da540f
liying.chen <liying.chen@asiainnovations.com>
2018-07-20 10:31:29 +0800

    1.0.0

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -59,7 +59,7 @@ if (env == 'stage') {
 var config = {
     entry: {
         // 可对应多个入口文件
-        webapp: ['babel-polyfill','./js/app.js'],
+        webapp: ['./js/app.js'],
         vendor: ['react', 'react-dom', 'react-router', 'mobx-react']
     },
     output: output,
