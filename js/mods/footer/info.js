b65bec8bb13db7e0e9440fad893e5052ba5658b1
haiyang.li <haiyang.li@asiainnovations.com>
2018-09-03 21:14:15 +0800

    footer群联系方式修改

--- js/mods/footer/info.js
+++ js/mods/footer/info.js
@@ -7,9 +7,9 @@ class InfoView extends Component {
                 <p className="phone">{UPEX.lang.template("CustomerServicePhone")}</p>
                 <p className="mail">{UPEX.lang.template("CustomerServiceEmail")}</p>
                 <div className="icons">
+                    <a href={UPEX.config.csurls.youtube} target="_blank"><i className="icon youtube"></i></a>
                     <a href={UPEX.config.csurls.telegram} target="_blank" ><i className="icon telegram"></i></a>
-                    <a href={UPEX.config.csurls.whatapp} target="_blank"><i className="icon whatapp"></i></a>
-                    {/* <a href={UPEX.config.csurls.line} target="_blank"><i className="icon line"></i></a> */}
+                    <a href={UPEX.config.csurls.linkedin} target="_blank"><i className="icon linkedin"></i></a>
                     <a href={UPEX.config.csurls.facebook} target="_blank"><i className="icon facebook"></i></a>
                     <a href={UPEX.config.csurls.twitter} target="_blank"><i className="icon twitter"></i></a>
                 </div>
