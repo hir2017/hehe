d5452f8cab768eadd73a8096010c9578d512fda1
haiyang.li <haiyang.li@asiainnovations.com>
2018-09-05 18:04:10 +0800

    澳洲版：身份认证证件类型去掉年龄证明

--- js/pages/user/idcard-auth-aus/getIdTypes.js
+++ js/pages/user/idcard-auth-aus/getIdTypes.js
@@ -2,7 +2,7 @@ export default function getIdTypes(type) {
     let types = [
         {value:'3', label: UPEX.lang.template('护照')},
         {value:'2', label: UPEX.lang.template('驾照')},
-        {value:'1', label: UPEX.lang.template('年龄证明')},
+        // {value:'1', label: UPEX.lang.template('年龄证明')},
     ]
     if(type) {
         return types.filter(item => item.value == type)
