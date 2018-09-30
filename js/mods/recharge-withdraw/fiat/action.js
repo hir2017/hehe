299c017e4adb704ae00376e39720367b5dc511a4
haiyang.li <haiyang.li@asiainnovations.com>
2018-09-30 11:50:58 +0800

    调整智付通充值限制测试

--- js/mods/recharge-withdraw/fiat/action.js
+++ js/mods/recharge-withdraw/fiat/action.js
@@ -30,7 +30,7 @@ export default (store) => {
                 return false;
             }
             // 500
-            if (state.amount < 500) {
+            if (state.amount < 14) {
                 message.error(UPEX.lang.template('最小充值金额为500'));
                 return false;
             }
