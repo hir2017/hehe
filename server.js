2d30e1210a284a862d5f9ea7f691b84854d0d2e2
liying.chen <liying.chen@asiainnovations.com>
2018-10-22 21:16:43 +0800

    landpage页面

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var express = require('express');
+var compression = require('compression');
 var webpack = require('webpack'),
     webpackDevMiddleware = require('webpack-dev-middleware'),
     webpackHotMiddleware = require('webpack-hot-middleware'),
@@ -12,6 +13,7 @@ var port =  process.env.PORT || 3000;
 var compiler = webpack(webpackDevConfig);
 var app = express();
 
+app.use(compression());
 app.use(history()); // 页面刷新的时候不至于404
 
 app.use(webpackDevMiddleware(compiler, {
