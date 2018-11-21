9ee76710ce8afdbc5f8ff62ba29a95a567b13e0c
haiyang.li <haiyang.li@asiainnovations.com>
2018-11-21 18:21:40 +0800

    资产变动记录bugifx

--- js/pages/record-list/assets-change/action.js
+++ js/pages/record-list/assets-change/action.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Icon} from 'antd';
 import TimeUtil from '@/lib/util/date';
 
 import { getFundChangeList, ausGetFundChangeList, getCoinRechargeList, getCoinWithdrawList, getAssetChangeReward } from '@/api/http';
