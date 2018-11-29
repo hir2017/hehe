52276ec8bf2e19d9b664e155cbf376549da99613
ShangJin <jin.shang@asiainnovations.com>
2018-11-29 17:24:31 +0800

    IEO倒计时添加

---
+++ js/pages/ieo/countdown.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview 倒计时
+ * @Date: 2018/11/29
+ */
+
+import React, {Component} from 'react';
+import Timer from 'c-timer';
+
+class CountDown extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        this.createCountDown();
+    }
+
+    createCountDown() {
+        let remainTime = this.props.remainTime;
+
+        if (remainTime > 0) {
+            this.countdown = new Timer({
+                remainTime: remainTime,
+                selector: {
+                    day: '#J-Countdown .day',
+                    hour: '#J-Countdown .hour',
+                    minute: '#J-Countdown .minute',
+                    second: '#J-Countdown .second'
+                }
+            });
+
+            //倒计时结束进行的操作
+            this.countdown.on('end', () => {
+
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="countdown" id="J-Countdown">
+                <div className="main">
+                    <div className="day-wrap">
+                        <em className="day">00</em>
+                        <div>{UPEX.lang.template('天')}</div>
+                    </div>
+                    <div className="hour-wrap">
+                        <em className="hour">00</em>
+                        <div>{UPEX.lang.template('小时')}</div>
+                    </div>
+                    <div className="minute-wrap">
+                        <em className="minute">00</em>
+                        <div>{UPEX.lang.template('分')}</div>
+                    </div>
+                    <div className="second-wrap">
+                        <em className="second">00</em>
+                        <div>{UPEX.lang.template('秒')}</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default CountDown;
--- js/pages/ieo/main/list.js
+++ js/pages/ieo/main/list.js
@@ -2,31 +2,84 @@
  * @fileoverview IEO 数字币列表
  */
 import React, {Component} from 'react';
+import CountDown from '../countdown';
 
 class View extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [],
+            isFetching: 0,
+            list: [
+                {
+                    symbol: 'GIFTO',
+                    numDesc: 'Amount',
+                    projectType: 1,
+                    purchase: false,
+                    purchasedNumber: 12500000,
+                    purchasedCount: 1011,
+                    projectStatusName: 'Success',
+                    feeRate: 0.0,
+                    picPath: 'https://ex.bnbstatic.com/file/resources/img/20171212/image_1513078271488.jpg',
+                    projectStatus: '45',
+                    number: 15000000,
+                    projectDetailDesc:
+                        '<img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184332_490.jpg" width="1200" height="876" alt="" /><img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184627_75.jpg" width="1200" height="1124" alt="" /><img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184411_971.jpg" width="1200" height="354" alt="" /><img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184422_574.jpg" width="1200" height="856" alt="" /><img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184433_777.jpg" width="1200" height="902" alt="" /><img src="https://glht.bjadm.net/file/resources/img/kindeditor/attached/image/20171214/20171214064224_4.jpg" width="1200" height="1025" alt="" /><img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184453_571.jpg" width="1200" height="771" alt="" /><img src="https://resource.binance.com/file/resources/img/kindeditor/attached/image/20171212/20171212184505_250.jpg" width="1200" height="640" alt="" />',
+                    price: 0.035,
+                    timeDesc: 'End Time',
+                    projectDesc:
+                        'GIFTO Protocol, a new blockchain protocol that will integrate seamlessly with any content platform to allow content creators and influencers to generate income with virtual gifts.  Gifto Protocol will be powered by Gifto tokens.',
+                    distributeNum: 15000000,
+                    time: 1513857600000,
+                    asset: 'BNB',
+                    projectName: 'GIFTO-BNB Session',
+                    projectId: '19',
+                    priceDesc: 'Price'
+                }
+            ],
         }
     }
 
     componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        this.setState({
+            isFetching: 1
+        });
+
+        // getIEOList().then(res => {
+        //     if (res.status == 200) {
+        //         this.setState({
+        //             list: res.attachment,
+        //             isFetching: 0
+        //         })
+        //     }
+        // }).catch(()=>{
+        //     console.log('getIEOList err')
+        //     this.setState({
+        //         isFetching: 0
+        //     })
+        // })
     }
 
     render() {
         let $content = null;
         $content = this.state.list.map((item, i) => (
-            <li className="token-item">
+            <li className="token-item" key={i}>
                 <a href="">
                     <div className="token-pic">
-                        <img src={item.pic} alt=""/>
+                        <img src={item.picPath} alt=""/>
                     </div>
                     <div className="token-status"></div>
                     <div className="token-content">
                         <h3 className="name"></h3>
                         <p className="desc"></p>
-                        <div className="progress"></div>
+                        <div className="progress">
+                            {
+                                item.projectType == 1 ? <CountDown remainTime="100000"/> : <div>已募集。。。</div>
+                            }
+                        </div>
                         <div className="detail">
                             <div className="amount"></div>
                             <div className="time"></div>
@@ -46,3 +99,5 @@ class View extends Component {
 }
 
 export default View;
+
+
