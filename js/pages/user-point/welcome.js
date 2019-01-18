0a8489383eec9b3bccb6b012696ff6d8aed5d940
ShangJin <jin.shang@asiainnovations.com>
2019-01-18 10:56:26 +0800

    ace: AP落地页开发

--- js/pages/user-point/welcome.js
+++ js/pages/user-point/welcome.js
@@ -6,78 +6,78 @@
 
 import '@/../css/user-point/index.less';
 import React, {Component} from "react";
-import UserPointInfo from './user';
 import {observer, inject} from 'mobx-react';
 import {getUserPointInfo} from "@/api/http";
 import {browserHistory} from "react-router";
 
-@inject('authStore', 'userInfoStore')
+@inject('authStore')
 @observer
 class PageView extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            userInfo: {}
-        }
-    }
-
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData() {
-        if (!this.props.authStore.isLogin) {
-            return;
-        }
-        getUserPointInfo().then((res) => {
-            if (res.status == 200) {
-                this.setState({
-                    userInfo: res.attachment
-                })
-            }
-
-        })
     }
 
     handleLogin = (e) => {
         browserHistory.push('/login');
     };
 
-    handleKYC = (e) => {
-        browserHistory.push('/user/authentication');
+    handlePoint = (e) => {
+        browserHistory.push('/user-point/home');
     };
 
-
     render() {
-        let {userInfo} = this.state;
-        let {authStore, userInfoStore} = this.props;
-        let $content;
+
+        let {authStore} = this.props;
+        let $btn;
 
         if (!authStore.isLogin) {
-            $content = <div className="no-login">
-                {UPEX.lang.template('您尚未登录，请登录后使用AcePoint')}
-                <button type="button" className="login-btn"
-                        onClick={this.handleLogin}>{UPEX.lang.template('登录')}</button>
-            </div>
+            $btn = <button type="button" className="login-btn"
+                           onClick={this.handleLogin}>{UPEX.lang.template('登录')}</button>
+
         } else {
-            if (userInfoStore.userInfo.authLevel == 0) {
-                $content = [
-                    <UserPointInfo data={userInfo} key="userinfo"/>,
-                    <div className="no-kyc" key="kycinfo">
-                        {UPEX.lang.template('您的积分需要通过身份认证后方可使用，点击跳转')}
-                        <button type="button" className="kyc-btn"
-                                onClick={this.handleKYC}>{UPEX.lang.template('身份认证')}</button>
-                    </div>];
-            } else {
-                $content = <UserPointInfo data={userInfo}/>
-            }
+            $btn = <button type="button" className="login-btn"
+                           onClick={this.handlePoint}>{UPEX.lang.template('查看我的ACE Point')}</button>
 
         }
 
         return (
             <div className="user-point welcome">
-                {$content}
-                BANNER
+                <div className="content">
+                    <div className="desc-wrap">
+                        <div className="desc right">
+                            <div className="title">{UPEX.lang.template('什么是ACE Point')}</div>
+                            <div className="txt"
+                                 dangerouslySetInnerHTML={{__html: UPEX.lang.template('什么是ACE Point内容')}}></div>
+                        </div>
+                        <div className="desc left">
+                            <div className="title">{UPEX.lang.template('有了ACE Point之后')}</div>
+                            <div className="txt"
+                                 dangerouslySetInnerHTML={{__html: UPEX.lang.template('有了ACE Point之后内容')}}></div>
+                        </div>
+                    </div>
+                    <div className="detail-wrap">
+                        <div className="title">{UPEX.lang.template('如何获得ACE Point')}</div>
+                        <div className="detail-txt">{UPEX.lang.template('如何获得ACE Point内容')}</div>
+                        <ul>
+                            <li className="way">{UPEX.lang.template('进行有手续费的交易活动')}</li>
+                            <li className="way">{UPEX.lang.template('每日登入 ACE')}</li>
+                            <li className="way">{UPEX.lang.template('完成新手任务')}</li>
+                            <li className="way">{UPEX.lang.template('邀请朋友注册ACE')}</li>
+                        </ul>
+                        <ul>
+                            <li className="way">{UPEX.lang.template('问题回报')}</li>
+                            <li className="way">{UPEX.lang.template('参加交易所活动')}</li>
+                            <li className="way">{UPEX.lang.template('数位资产购买')}</li>
+                            <li className="way">{UPEX.lang.template('其他神秘挑战')}</li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div className="footer">
+                    <p className="greet">{UPEX.lang.template('底部欢迎语1')}</p>
+                    <p>{UPEX.lang.template('底部欢迎语2')}</p>
+                    {$btn}
+                </div>
             </div>
         );
 
