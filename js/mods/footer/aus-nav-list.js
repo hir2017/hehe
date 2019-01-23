9b58fd17d85c6ae51328a83a5216fd430b1d6134
ShangJin <jin.shang@asiainnovations.com>
2019-01-23 15:56:47 +0800

    ace: 澳洲台湾footer链接文件拆分

--- js/mods/footer/aus-nav-list.js
+++ js/mods/footer/aus-nav-list.js
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview: footer条款链接-澳洲
+ * @author: ShangJin
+ * @date: 2019/1/23
+ */
 import React, {Component} from 'react';
 import {inject} from 'mobx-react';
 import {Link, browserHistory} from 'react-router';
@@ -8,15 +13,10 @@ const getNavList = () => {
         [
             {
                 name: UPEX.lang.template('关于我们'),
-                path: UPEX.lang.template('关于我们网页链接')
             },
             {
                 name: UPEX.lang.template('关于网站'),
                 path: UPEX.lang.template('关于网站网页链接')
-            },
-            {
-                name: UPEX.lang.template('团队介绍'),
-                path: UPEX.lang.template('团队介绍网页链接')
             },
             {
                 name: UPEX.lang.template('联系我们'),
@@ -26,7 +26,6 @@ const getNavList = () => {
         [
             {
                 name: UPEX.lang.template('服务支持'),
-                path: UPEX.lang.template('服务支持网页链接')
             },
             {
                 name: UPEX.lang.template('问题反馈'),
@@ -39,38 +38,39 @@ const getNavList = () => {
             {
                 name: UPEX.lang.template('费率说明'),
                 path: UPEX.lang.template('费率说明网页链接')
+            },
+            {
+                name: UPEX.lang.template('投诉机制'),
+                path: UPEX.lang.template('投诉机制网页链接')
             }
         ],
         [
             {
                 name: UPEX.lang.template('客户声明'),
-                path: UPEX.lang.template('客户声明网页链接')
             },
             {
                 name: UPEX.lang.template('隐私条款'),
-                path: UPEX.lang.template('隐私条款网页链接'),
+                path: UPEX.lang.template('隐私条款网页链接')
             },
             {
                 name: UPEX.lang.template('免责声明'),
-                path: UPEX.lang.template('免费声明网页链接'),
+                path: UPEX.lang.template('免费声明网页链接')
             },
             {
                 name: UPEX.lang.template('用户协议'),
                 path: UPEX.lang.template('用户协议网页链接'),
+            },
+            {
+                name: UPEX.lang.template('AML&CTF Program'),
+                path: UPEX.lang.template('AML&CTF Program网页链接')
             }
         ],
         [
             {
-                name: UPEX.lang.template('上币申请'),
-                path: UPEX.config.docUrls.applyCurrency
+                name: UPEX.lang.template('下载客户端'),
+                path: UPEX.lang.template('下载客户端链接')
             }
-        ],
-        // [
-        //     {
-        //         name: UPEX.lang.template('下载客户端'),
-        //         route: '/download-app'
-        //     }
-        // ]
+        ]
     ];
 };
 
@@ -82,7 +82,6 @@ class NavsView extends Component {
         this.navs = navs;
     }
 
-
     render() {
         return (
             <ul className="nav-list">
@@ -90,15 +89,27 @@ class NavsView extends Component {
                     return (
                         <li key={i}>
                             {items.map((item, j) => {
-                                // 外链跳转
-                                if (item.path) {
-                                    return (
-                                        <a className="nav-item" key={j} target="_blank" href={item.path}>
-                                            {item.name}
-                                        </a>
-                                    );
-
+                                if(item.path){
+                                    // 外链跳转，如果是每组第一个则不支持跳转，除了跳转客户端
+                                    if (i == 3) {
+                                        return (
+                                            <a className="nav-item" key={j} target="_blank" href={item.path}>
+                                                {item.name}
+                                            </a>
+                                        );
+                                    } else {
+                                        return j === 0 ? (
+                                            <a className="nav-item" key={j}>
+                                                {item.name}
+                                            </a>
+                                        ) : (
+                                            <a className="nav-item" key={j} target="_blank" href={item.path}>
+                                                {item.name}
+                                            </a>
+                                        );
+                                    }
                                 }
+
                                 // 本地跳转
                                 return (
                                     <Link className="nav-item" key={j} to={`/${item.route}`}>
